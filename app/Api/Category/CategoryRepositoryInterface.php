c03c52645e1f49e0debd04d97baa28cb9fdde7ac
Alessandro Scetto <alescetto03@gmail.com>
2020-05-14 01:04:54 +0200

    14/05/2020

---
+++ app/Api/Category/CategoryRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Api\Category;
+
+
+class CategoryRepositoryInterface
+{
+
+}
---
+++ app/Api/Product/ProductRepositoryInterface.php
@@ -0,0 +1,10 @@
+<?php
+
+
+namespace App\Api\Product;
+
+
+class ProductRepositoryInterface
+{
+
+}
